64e4ed5fb549d9dcea354ac328370f656b7121a5
phi <phi1618jp@gmail.com>
2014-01-24 14:44:05 +0900

    build

--- tmlib.novel.js
+++ tmlib.novel.js
@@ -96,6 +96,7 @@ tm.define("tm.novel.Script", {
                     else if (tag_flag == false && ch == "[") {
                         if (tasks.last && tasks.last.type == "text") {
                             tasks.last.value += text;
+                            text = "";
                         }
                         else {
                             tasks.push({
@@ -113,6 +114,7 @@ tm.define("tm.novel.Script", {
                 if (text != "") {
                     if (tasks.last && tasks.last.type == "text") {
                         tasks.last.value += text;
+                        text = "";
                     }
                     else {
                         tasks.push({
